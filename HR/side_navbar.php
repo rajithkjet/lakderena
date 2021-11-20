b1339cbd12478ea6247e78c7bdb28cc8637e7c2f
kavzy <kaveesha.s.ekanayake@gmail.com>
2021-11-21 01:17:08 +0530

    hr dashboard updated

---
+++ HR/side_navbar.php
@@ -0,0 +1,81 @@
+<!-- Sidenav -->
+  <nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
+    <div class="scrollbar-inner">
+      <!-- Brand -->
+      <div class="sidenav-header  align-items-center">
+        <a class="navbar-brand" href="javascript:void(0)">
+          <img src="../images/lakderana7.png" class="navbar-brand-img" alt="...">
+        </a>
+      </div>
+      <div class="navbar-inner">
+        <!-- Collapse -->
+        <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+          <!-- Nav items -->
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" href="/admin/">
+                <i class="fas fa-tachometer-alt text-primary"></i>
+                <span class="nav-link-text">Dashboard</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="fas fa-users text-green"></i>
+                <span class="nav-link-text">Manage Employee Details</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="fas fa-briefcase text-pink"></i>
+                <span class="nav-link-text">Manage Employee Attendance</span>
+              </a>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="fas fa-walking text-orange"></i>
+                <span class="nav-link-text">Manage Employee Leaves</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="fas fa-money-bill-wave text-blue"></i>
+                <span class="nav-link-text">Calculate Employee Salary</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+              <i class="fas fa-file-invoice-dollar text-green"></i>
+                <span class="nav-link-text">Send Salary Script</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="fas fa-briefcase text-pink"></i>
+                <span class="nav-link-text">Employee Attendance</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="fas fa-money-check-alt text-orange"></i>
+                <span class="nav-link-text">Payroll Reports</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="ni ni-settings text-orange"></i>
+                <span class="nav-link-text">Account Settings</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="ni ni-button-power text-dark"></i>
+                <span class="nav-link-text">Logout</span>
+              </a>
+            </li>
+          </ul>
+         
+      
+        </div>
+      </div>
+    </div>
+  </nav>
