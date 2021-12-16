51fcb6b6df24959dd3be3bf825f6c7a044ddc924
kavzy <kaveesha.s.ekanayake@gmail.com>
2021-12-16 22:02:40 +0530

    update customers

--- Receptionist/side_navbar.php
+++ Receptionist/side_navbar.php
@@ -36,7 +36,7 @@
               </a>
             </li>
              <li class="nav-item">
-              <a class="nav-link" href="#">
+              <a class="nav-link" href="update_customers.php">
                 <i class="fas fa-user-edit text-orange"></i>
                 <span class="nav-link-text">Update Existing Customer</span>
               </a>
---
+++ Receptionist/update_customers.php
@@ -0,0 +1,82 @@
+<?php
+    session_start();
+    if(!(isset($_SESSION['role_id'])) && !(isset($_SESSION['username']))){
+        header("location:../index.php");
+    }
+    if ($_SESSION['role_id'] != 2) {
+        header("location:../index.php");
+    }  
+
+    $user = $_SESSION['username'];
+
+?>
+<!DOCTYPE html>
+<html>
+
+<!-- Header -->
+<?php include "../dashboard_header.php" ?>
+
+<body>
+  <!-- Sidenav --> 
+<?php include "side_navbar.php" ?>
+  <!-- Main content -->
+  <div class="main-content" id="panel">
+ 
+  <!-- Topnav -->
+  <?php include "top_navbar.php" ?>
+    <!-- Header -->
+    <!-- Header -->
+    <div class="header bg-gradient-orange pb-6">
+      <div class="container-fluid">
+        <div class="header-body">
+          <div class="row align-items-center py-4">
+            <div class="col-lg-6 col-7">
+              <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
+                <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
+                  <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
+                  <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">Update Customers</li>
+                </ol>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Page content -->
+    <div class="container-fluid mt--6">
+      <div class="row justify-content-center">
+        <div class=" col ">
+          <div class="card">
+            <div class="card-header bg-transparent">
+              <h3 class="mb-0">Update Customers</h3>
+            </div>
+            <div class="card-body">
+              <div class="row icon-examples">
+             
+             <!-- Add  Content here  -->
+
+             <p>Add Content Here!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Footer -->
+      <?php include "../dashboard_footer.php" ?>
+    </div>
+  </div>
+  <!-- Argon Scripts -->
+  <!-- Core -->
+  <script src="../assets/vendor/jquery/dist/jquery.min.js"></script>
+  <script src="../assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/vendor/js-cookie/js.cookie.js"></script>
+  <script src="../assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+  <script src="../assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+  <!-- Optional JS -->
+  <script src="../assets/vendor/clipboard/dist/clipboard.min.js"></script>
+  <!-- Argon JS -->
+  <script src="../assets/js/argon.js?v=1.2.0"></script>
+</body>
+
+</html>
